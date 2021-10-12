8bd97e57152075dfffc1473a2edb26f6d2644b8d
arioass <arisson.albuquerque@gmail.com>
2021-10-12 08:50:50 -0300

    primeiro teste junit

---
+++ src/main/java/br/com/alura/carteira/service/CalculadoraDeImpostoService.java
@@ -0,0 +1,32 @@
+package br.com.alura.carteira.service;
+
+import br.com.alura.carteira.modelo.TipoTransacao;
+import br.com.alura.carteira.modelo.Transacao;
+import org.apache.tomcat.jni.BIOCallback;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class CalculadoraDeImpostoService {
+
+    //15% de imposto para transacoes do tipo venda com valor superior a 20mil
+    public BigDecimal calcular(Transacao transacao){
+        if (transacao.getTipo() == TipoTransacao.COMPRA){
+            return BigDecimal.ZERO;
+        }
+
+        BigDecimal valorTransacao = transacao
+                .getPreco()
+                .multiply(new BigDecimal(transacao.getQuantidade()));
+
+        if (valorTransacao.compareTo(new BigDecimal(20000))< 0){
+            return BigDecimal.ZERO;
+
+        }
+
+        return valorTransacao
+                .multiply(new BigDecimal("0.15"))
+                .setScale(2, RoundingMode.HALF_UP);
+
+    }
+}
